abbbfc87ca75447bef35b2bbed19724e64703968
gabrielcaio11 <gabrielcaio848@gmail.com>
2024-07-04 12:11:01 -0300

    Exemplo

--- Sprint1/EstruturasDeControle/src/exemplo/Main.java
+++ Sprint1/EstruturasDeControle/src/exemplo/Main.java
@@ -20,23 +20,31 @@ public class Main {
 		case 1:
 		case 2:
 			estacao = "Verão";
+			break;
 		case 3:
 			estacao = (dia < 21) ? "Verão" : "Outono";
+			break;
 		case 4:
 		case 5:
 			estacao = "Outono";
+			break;
 		case 6:
 			estacao = (dia < 21) ? "Outono" : "Inverno";
+			break;
 		case 7:
 		case 8:
 			estacao = "Inverno";
+			break;
 		case 9:
 			estacao = (dia < 23) ? "Inverno" : "Primavera";
+			break;
 		case 10:
 		case 11:
 			estacao = "Primavera";
+			break;
 		case 12:
 			estacao = (dia < 22) ? "Primavera" : "Verão";
+			break;
 		default:
 			estacao = "Mês inválido";
 		}
---
+++ Sprint2/.metadata/.plugins/org.eclipse.core.resources/.history/dd/70b9490a163a001f1aabea052031a3c7
@@ -0,0 +1,9 @@
+package entities;
+
+public class Cliente extends Pessoa {
+
+
+	public Cliente(String nome, String cpf, String cep) {
+		super(nome, cpf, cep);
+	}
+}
